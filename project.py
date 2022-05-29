f586f87ad8100ecef4a8b975149adf450cf084e6
Andrico Cahyadi <ricocahyadi777.dev@gmail.com>
2022-05-30 00:07:58 +0800

    Create project.py

---
+++ project.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd;
+import numpy as np;
+
+
+#Utility method takes in daily % pnl vector and computes portfolio diagnostics
+#daily % pnl is inputed as a dataframe, with date as index and corresponding pnl for that date as the value
+def portfolioDiagnostics(signal_df, label):
+    #NET SECTOR EXPOSURES
+    import matplotlib.pyplot as plt
+    plt.figure();
+    sector_df = pd.read_csv("sector.csv", error_bad_lines = False)
+    d = sector_df.set_index('ticker').to_dict()
+    signal_df2 = signal_df.copy(deep=True)
+    signal_df2.columns = signal_df2.columns.to_series().map(d['sector'])
+    uniquesector = list(set(signal_df2.columns))
+    for tsector in uniquesector:
+        if (str(tsector)=='nan'):
+            continue;
+        tmean = signal_df2.loc[:, tsector].sum(axis=1)
+        tmean[~np.isfinite(tmean)] = 0
+        plt.plot(tmean.values, label=tsector)
+        plt.legend();
+        plt.title(label+" NET EXPOSURE")
+        plt.show(block=False)
+    return
+
+
+#Utility method takes in daily % pnl vector and computes pnl diagnostics
+#daily % pnl is inputed as a dataframe, with date as index and corresponding pnl for that date as the value
+def pnlPerformance(pnl, label):
+    cumpnl = pnl.cumsum(skipna = True)
+    import matplotlib.pyplot as plt
+    import numpy as np
+    sharpe = pnl.mean()/np.std(pnl)
+    sharpe = sharpe*np.sqrt(252)
+    print("")
+    print ("PERFORMANCE STATISTICS FOR "+label);
+    print("Daily annualized sharpe: "+str(sharpe))
+    print ("Average annual returns: "+str(pnl.mean()*252*100)+"%")
+    print ("Total returns: "+str(pnl.sum()*100)+"%");
+    highwatermark_df = cumpnl.cummax();
+    drawdown_df = cumpnl - highwatermark_df;
+    maxdrawdown = drawdown_df.min();
+    print ("Max drawdown: "+str(maxdrawdown*100)+"%");
+    plt.plot(cumpnl.values, label = label);
+    plt.legend();
+    plt.show(block=False);
+    plt.title("Cumulative PNL chart")
+    #HERE, we compute performance during 'stressed' historical periods
+    stressedmarkets = dict()
+    stressedmarkets["Covid19"] = (20200301, 20200317);          #Market crash 1H March 2020
+    stressedmarkets["Dec18"] = (20181215, 20181231);            #Market crash last 2 weeks
+    stressedmarkets["Fall2015"] = (20150701, 20150901);         #Taper tantrum / EU debt crisis.  24 Aug 2015 was "BlackMonday" for Asian, EU and US markets
+    stressedmarkets["Oct14"] = (20141001, 20141031);            #Treasury flash crash on 15 Oct 2014
+    stressedmarkets["Aug2013"] = (20130820, 20130825);          #Flash freeze on 22 Aug 2013
+
+    for tkey in stressedmarkets.keys():
+        mask = pnl.index.to_series().between(stressedmarkets[tkey][0], stressedmarkets[tkey][1])
+        print("Stressed period return during "+tkey+":  "+str(pnl[mask].sum()*100)+"%")
+    print("===========================")
+    print("")
+
+#=======ACTUAL STRATEGY SCRIPT STARTS HERE============================#
+
+russell_df = pd.read_csv("russell2000pvdata.csv", error_bad_lines = False)
+vars = ['open', 'high', 'low', 'close', 'volume']
+rawdata = {}
+period = 20           #NUMBER OF TRADING DAYS TO COMPUTE MEAN REVERSION OVER.  THIS IS A PARAMETER FOR FITTING
+universesize = 2000                 #SIZE OF PORTFOLIO IN NUMBER OF STOCKS.  IF THIS IS MORE THAN 2000, IT WILL GENERALLY JUST BE CAPPED AT 2000 SINCE BASE UNIVERSE IS RUSSELL 2000
+maxindividualweight = 0.01          #MAXIMUM FRACTION A SINGLE POSITION CAN TAKE UP OF ENTIRE PORTFOLIO.  0.01 MEANS 1%.  i.e. if you have a portfolio of $100 million, max single position size is $1 million
+
+
+for tvar in vars:
+    rawdata[tvar] = russell_df.loc[:, ['tickerid', 'ticker', 'date', tvar]]
+    rawdata[tvar] = rawdata[tvar].pivot(index = 'date', columns = 'ticker', values = tvar)
+    rawdata[tvar] = rawdata[tvar].iloc[:, :universesize]
+    
+return_df = (rawdata['close'] / rawdata['close'].shift(1)) - 1
+closing_price = rawdata['close'].dropna(axis=1)
+#USING MA CROSSOVER:  MOMENTUM SIGNAL
+signal_df = -(return_df.rolling(3).mean() - return_df.rolling(period, min_periods = 3).mean())
+signal_df = signal_df.subtract(signal_df.mean(axis=1), axis='index')
+signal_df = signal_df.divide(signal_df.abs().sum(axis=1), axis='index')
+signal_df = signal_df.shift(1)                                              #TO AVOID FORWARD BIAS.  WE USE YESTERDAY'S INFORMATION TO EXECUTE AT TODAY'S CLOSE PRICES
+for i in range(3):
+    signal_df[signal_df > maxindividualweight] = maxindividualweight
+    signal_df[signal_df < -maxindividualweight] = -maxindividualweight
+    signal_df = signal_df.subtract(signal_df.mean(axis=1), axis='index')
+    signal_df = signal_df.divide(signal_df.abs().sum(axis=1), axis='index')
+pnl_df = signal_df * return_df.shift(-1)                                    #BASED ON YESTERDAY'S INFORMATION, WE EXECUTE AT TODAY'S CLOSE PRICES AND COMPUTE OUR PNL BASED ON TOMORROW'S RETURN
+pnl = pnl_df.sum(axis=1)
+#pnlPerformance(pnl, "MARKET NEUTRAL")
+#marketneutralportfolio = signal_df.copy(deep=True)
+
+#USING EMA CROSSOVER:  MOMENTUM SIGNAL
+ema_short = closing_price.ewm(span=5, adjust=False).mean()
+print("EMA SHORT")
+print(closing_price.head(40))
+print(ema_short.head(40))
+ema_long = closing_price.ewm(span=200, adjust=False).mean()
